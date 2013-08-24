1a550c60795b8f3129bb4e093f02d9f3531ba30d
RestlessMystic <govindmaheswaran@gmail.com>
2013-08-25 00:24:34 +0530

    Added Prog05 (String) and Prog06(List)

---
+++ prog05.py
@@ -0,0 +1,18 @@
+#
+#	Taming the Python
+#	Govind Maheswaran
+#	Example Programs
+
+
+#	Prog 05
+#	Program to demonstrate some String Operations
+
+str1 = raw_input("Enter first string : ")
+str2 = raw_input("Enter second string : ")
+
+print "Checking if second string is substring of circulated first string"
+str1 = str1 + str2
+if str2 in str1:
+	print "Yes"
+else:
+	print "No"
---
+++ prog06.py
@@ -0,0 +1,16 @@
+#
+#	Taming the Python
+#	Govind Maheswaran
+#	Example Programs
+
+
+#	Prog 06
+#	Program to demonstrate some list Operations
+
+a = list((1,6,2,9,4,3,8,5,7))
+print a
+a.sort()
+print a
+a.append(15)
+print a;
+print a[5]
