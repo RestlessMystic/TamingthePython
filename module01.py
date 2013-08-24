1deba45d70c2c21984f46483a4c597c7dd3d3758
RestlessMystic <govindmaheswaran@gmail.com>
2013-08-25 02:23:45 +0530

    Added Module01 (Module Demo)

---
+++ module01.py
@@ -0,0 +1,22 @@
+#
+#	Taming the Python
+#	Govind Maheswaran
+#	Example Programs
+
+
+#	Prog 09
+#	Program to demonstrate Modules
+
+def area_triangle(b,h):
+	area = b * h * 0.5
+	return(area)
+	
+def area_circle(r):
+	area = 3.14 * (r**2)
+	return(area)
+	
+def area_rect(l,b):
+	return(l*b)
+	
+def area_sqr(s):
+	return(s**2)
