3e56d97b3058a2da059eb790acf64416549c7aa2
RestlessMystic <govindmaheswaran@gmail.com>
2013-08-25 00:51:51 +0530

    Added Prog07(Set Operations)

---
+++ prog07.py
@@ -0,0 +1,17 @@
+#
+#	Taming the Python
+#	Govind Maheswaran
+#	Example Programs
+
+
+#	Prog 07
+#	Program to demonstrate some set Operations
+
+a = set("abracadabra")
+print a
+b = set("alacazam")
+print b
+print "a INTERSECTION b = ", a&b
+print "a UNION b = ", a|b
+print "a DIFFERENCE b = ", a-b
+print "a XOR b = ", a^b
